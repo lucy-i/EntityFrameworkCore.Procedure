4d83895bae10815213e71155ac6bf5ebd0a94a9a
lucy-i <lucy@ilucy.in>
2018-11-20 18:36:42 -0800

    Added DB Project as source for Testing

---
+++ Test.SQLDatabase/Stored Procedures/Sp_With_Param.sql
@@ -0,0 +1,36 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[Sp_With_Param]
+	(
+	@Id INT,
+	@Name Varchar(70),
+	@UpdatedDate DateTime
+	)			
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	IF EXISTS(SELECT TOP 1 1 FROM dbo.[SimpleModel] Where Id=@Id)
+	BEGIN
+		UPDATE SM 
+		SET SM.[Name]=@Name,
+			SM.[UpdatedDate]=@UpdatedDate ,
+			SM.[Key]=NEWID()
+		FROM dbo.[SimpleModel] SM
+		WHERE SM.Id=@Id
+	END
+	ELSE
+	BEGIN 
+		INSERT INTO [dbo].[SimpleModel] ([Name],[Key],[SimpleModelCode],[UpdatedDate],[RefreshTime])
+		   VALUES(@Name,NEWID(),@Name,@UpdatedDate,3000000000)
+	END
+END
+
+
+
---
+++ Test.SQLDatabase/Tables/SimpleModel.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SimpleModel] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Name]            VARCHAR (70)   NOT NULL,
+    [Key]             NVARCHAR (255) NULL,
+    [SimpleModelCode] VARCHAR (50)   NOT NULL,
+    [UpdatedDate]     DATETIME       NOT NULL,
+    [RefreshTime]     BIGINT         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
