ae850408c12736b801522ac1636dccff324f0e08
MasterRam <ramkumar.murugarasan@hotmail.com>
2018-11-26 19:48:00 -0800

    Added Sp almost all SQL datatypes as param

---
+++ Test.SQLDatabase/Stored Procedures/Sp_With_All_SQL_DataType_As_Params.sql
@@ -0,0 +1,43 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[Sp_With_All_SQL_DataType_As_Params]
+		(
+		@big_int BiGINT,
+		@int	int,
+		@small_int smallint,
+		@tiny_int tinyint,
+		@bit bit,
+		@decimal decimal(18,2),
+		@numeric numeric,
+		@money money,
+		@small_money smallmoney,
+		@float float(4),
+		@real real,
+		@date_time datetime,
+		@small_date_time smalldatetime,
+		@char char,
+		@varchar varchar(100),
+		@text text,
+		@nchar nchar(4),
+		@nvarchar nvarchar(4),
+		@ntext ntext,
+		@binary binary(5000),
+		@varbinary varbinary(4000),
+		@image image,
+		@timestamp timestamp,
+		@uniqueidentifier uniqueidentifier
+		)		
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+			
+END
+
+
+
---
+++ Test.SQLDatabase/Stored Procedures/Sp_With_Param_AnReturnList.sql
@@ -0,0 +1,23 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[Sp_With_Param_AnReturnList]
+	(
+	@Id INT,
+	@Name Varchar(70),
+	@UpdatedDate DateTime
+	)			
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	Select @Id Id, @Name [Name],@UpdatedDate [Date]
+END
+
+
+
